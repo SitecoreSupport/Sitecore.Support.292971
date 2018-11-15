a4bb73aba03e7788cf866229ae652f63b47f01ca
Vladimir Taran <vlt@sitecore.net>
2018-11-15 18:11:03 -0500

    Enabling the Owin config file gives error in preview mode on XP setups

---
+++ src/Sitecore.Support.292971/LargeDropDownButtonChildItems.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.SecurityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LargeDropDownButton
+{
+  public class LargeDropDownButtonChildItems : Sitecore.ExperienceEditor.Speak.Ribbon.Requests.LargeDropDownButton.LargeDropDownButtonChildItems
+  {
+    public override PipelineProcessorResponseValue ProcessRequest()
+    {
+      PipelineProcessorResponseValue chidList;
+      using (new SecurityDisabler())
+      {
+        chidList = base.ProcessRequest();
+      }
+      return chidList;
+    }
+  }
+}
---
+++ src/Sitecore.Support.292971/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.292971")]
+[assembly: AssemblyProduct("Sitecore.Support.292971")]
+[assembly: ComVisible(false)]
